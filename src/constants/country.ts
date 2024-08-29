7ee76d7cf254ccde1b18203724656a003fb3009d
Khoo Ven Jin <venjin_boy@hotmail.com>
2024-08-29 17:26:40 +0800

    feat: add server-list response

---
+++ src/constants/country.ts
@@ -0,0 +1,10 @@
+const DEFAULT_SERVER_LIST = {
+  country: "Singapore",
+  region: "SEA",
+  icon: "https://vpn-wireguard.s3.ap-southeast-1.amazonaws.com/images/sg-flag.png",
+  isDefault: true,
+}
+
+const SERVER_LIST = [DEFAULT_SERVER_LIST]
+
+export { DEFAULT_SERVER_LIST, SERVER_LIST }
