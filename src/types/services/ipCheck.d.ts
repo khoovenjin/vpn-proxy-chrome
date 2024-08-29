be88a2996dd34ebcbce1e131ffa6ea4e1a8475b8
Khoo Ven Jin <venjin_boy@hotmail.com>
2024-08-29 17:20:54 +0800

    feat: add ipCheck api typing

---
+++ src/types/services/ipCheck.d.ts
@@ -0,0 +1,12 @@
+type TIpCheckRequest = {
+  params: {
+    format: string
+  }
+}
+
+type TIpCheckResponse = string
+
+type TIpCheck = {
+  request: TIpCheckRequest
+  response: TIpCheckResponse
+}
